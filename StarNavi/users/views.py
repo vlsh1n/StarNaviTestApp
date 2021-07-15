4858f1c1a845c7caa8e07c09726b67351f644e29
Vladislav Voloshin <voloshinw@gmail.com>
2021-07-15 17:36:21 +0300

    created views for registration, login and logout logic for users app

--- StarNavi/users/views.py
+++ StarNavi/users/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.contrib.auth import login, logout
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from .forms import UserRegisterForm, UserLoginForm
+
+
+class Register(CreateView):
+    form_class = UserRegisterForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, **kwargs):
+        context = super(Register, self).get_context_data()
+        context['title'] = 'Registration'
+        return context
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserLoginForm
+    return render(request, 'users/login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('home')
+
