e21f62735a4eb045ced102db4755e68d59adeb06
Vladislav Voloshin <voloshinw@gmail.com>
2021-07-15 19:12:40 +0300

    created form for add movie page

---
+++ StarNavi/imdb/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+from .models import Movie
+
+
+class AddMovieForm(forms.ModelForm):
+    title = forms.CharField(label='Title', widget=forms.TextInput(attrs={'class': 'form-control'})),
+    genre = forms.ChoiceField(label='Genre', widget=forms.Select(attrs={'class': 'form-control'})),
+    release_date = forms.DateField(label='Release Date', widget=forms.SelectDateWidget(attrs={'class': 'form-control'})),
+    description = forms.CharField(label='Description', widget=forms.Textarea(attrs={'class': 'form-control'})),
+    duration = forms.IntegerField(label='Duration', widget=forms.NumberInput(attrs={'class': 'form-control'})),
+    rating = forms.IntegerField(label='Rating', widget=forms.NumberInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = Movie
+        fields = ('title', 'genre', 'release_date', 'description', 'duration', 'rating')
