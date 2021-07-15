86f696aa843a4c84b7706a6bf681237f1105265c
Vladislav Voloshin <voloshinw@gmail.com>
2021-07-15 19:09:31 +0300

    added path for add movie page

--- StarNavi/imdb/urls.py
+++ StarNavi/imdb/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import MainPage, MovieDetail, MoviesByGenre
+from .views import MainPage, MovieDetail, MoviesByGenre, AddMovie
 
 urlpatterns = [
     path('', MainPage.as_view(), name='home'),
     path('genres/<int:genre_id>', MoviesByGenre.as_view(), name='genres'),
     path('movie/<int:pk>', MovieDetail.as_view(), name='movie'),
+    path('add-movie', AddMovie.as_view(), name='addmovie')
 ]
