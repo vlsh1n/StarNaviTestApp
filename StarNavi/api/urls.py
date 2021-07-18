3202195d73590801a27eb0efe0ba66fa0170c10f
Vladislav Voloshin <voloshinw@gmail.com>
2021-07-18 17:55:42 +0300

    created router for user api

--- StarNavi/api/urls.py
+++ StarNavi/api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
 
-from .views import MovieListView, GenreListView, MovieDetailView, GenreDetailView
+from .views import MovieListView, GenreListView, MovieDetailView, GenreDetailView, UserViewSet
+
+router = DefaultRouter()
+router.register('users', UserViewSet, basename='users')
 
 
 urlpatterns = [
@@ -11,3 +15,4 @@ urlpatterns = [
     path('genres/<int:pk>', GenreDetailView.as_view(), name='genre_detail')
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
+urlpatterns += router.urls
