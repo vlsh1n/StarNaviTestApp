55acb546898398d3c5125c6bb120bffe546e5aa3
Vladislav Voloshin <voloshinw@gmail.com>
2021-07-18 19:08:33 +0300

    added settings for lifetime access and refresh tokens

--- StarNavi/StarNavi/settings.py
+++ StarNavi/StarNavi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+from datetime import timedelta
 from pathlib import Path
 from environs import Env
 
@@ -148,4 +149,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication'
     )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1)
 }
