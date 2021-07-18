e92a6bc17a2d0fcb154b8dd58407548d322d6f1c
Vladislav Voloshin <voloshinw@gmail.com>
2021-07-18 18:25:36 +0300

    added logic for hashing password when create new user with api

--- StarNavi/api/serializers.py
+++ StarNavi/api/serializers.py
@@ -33,7 +33,9 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
+        password = validated_data.pop('password', '')
         user = self.Meta.model(**validated_data)
+        user.set_password(password)
         user.save()
         return user
 
