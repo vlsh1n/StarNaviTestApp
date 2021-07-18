1a682beff6a5388f39a5efa0d3c94460a317d60c
Vladislav Voloshin <voloshinw@gmail.com>
2021-07-18 18:53:53 +0300

    added paths for jwt logic

--- StarNavi/StarNavi/urls.py
+++ StarNavi/StarNavi/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('imdb.urls')),
     path('users/', include('users.urls')),
     path('api/', include('api.urls')),
-    path('api-auth/', include('rest_framework.urls', 'rest_framework'))
+    path('api-auth/', include('rest_framework.urls', 'rest_framework')),
+    path('api/token/', TokenObtainPairView.as_view(), 'token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), 'token_refresh')
 ]
