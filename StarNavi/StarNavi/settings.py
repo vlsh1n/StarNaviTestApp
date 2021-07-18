bbebdfaf8de9faddb0516d20fc6bbf1202e3558b
Vladislav Voloshin <voloshinw@gmail.com>
2021-07-18 18:49:21 +0300

    added jwt authentication to project settings

--- StarNavi/StarNavi/settings.py
+++ StarNavi/StarNavi/settings.py
@@ -143,3 +143,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
+    )
+}
