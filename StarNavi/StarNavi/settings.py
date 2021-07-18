3c4af7c5d14ec1d96a173be977fb42b93d515674
Vladislav Voloshin <voloshinw@gmail.com>
2021-07-18 19:11:16 +0300

    return basic api authentication to settings

--- StarNavi/StarNavi/settings.py
+++ StarNavi/StarNavi/settings.py
@@ -147,8 +147,8 @@ MEDIA_URL = '/media/'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.BasicAuthentication'
     )
 }
 
