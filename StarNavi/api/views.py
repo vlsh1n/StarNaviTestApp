0a9878f27f9952463080cf16ad2d3653d37419e7
Vladislav Voloshin <voloshinw@gmail.com>
2021-07-19 11:15:41 +0300

    changed MovieListView and MovieDetailView to MovieViewSet

--- StarNavi/api/views.py
+++ StarNavi/api/views.py
@@ -18,26 +18,7 @@ from rest_framework.generics import GenericAPIView, RetrieveUpdateDestroyAPIView
 
 from .permissions import IsAuthorOrReadOnly
 
-
-class MovieListView(views.APIView):
-    permission_classes = (IsAuthorOrReadOnly, )
-
-    def perform_create(self, serializer):
-        return serializer.save(author=self.request.user)
-
-    def get(self, request, format=None):
-        movies = Movie.objects.all()
-        context = {'request': request}
-        serializer = MovieSerializer(movies, many=True, context=context)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        context = {'request': request}
-        serializer = MovieSerializer(data=request.data, context=context)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+from.mixins import LikesMixin
 
 
 class GenreListView(ListModelMixin, CreateModelMixin, GenericAPIView):
@@ -51,36 +32,6 @@ class GenreListView(ListModelMixin, CreateModelMixin, GenericAPIView):
         return self.create(request, *args, **kwargs)
 
 
-class MovieDetailView(views.APIView):
-    permission_classes = (IsAuthorOrReadOnly, )
-
-    def get_object(self, pk):
-        try:
-            return Movie.objects.get(pk=pk)
-        except Movie.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-    def get(self, request, pk, format=None):
-        movie = self.get_object(pk)
-        context = {'request': request}
-        serializer = MovieSerializer(movie, context=context)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        movie = self.get_object(pk)
-        context = {'request': request}
-        serializer = MovieSerializer(movie, context=context, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        movie = self.get_object(pk)
-        movie.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
 class GenreDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Genres.objects.all()
     serializer_class = GenreSerializer
@@ -91,3 +42,12 @@ class UserViewSet(ModelViewSet):
     queryset = model.objects.all()
     serializer_class = UserSerializer
     permission_classes = (IsAdminUser, )
+
+
+class MovieViewSet(LikesMixin, ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    permission_classes = (IsAuthorOrReadOnly, )
+
+    def perform_create(self, serializer):
+        return serializer.save(author=self.request.user)
