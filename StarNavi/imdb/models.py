acd5418bb62d6381559700c235fa46e743fff092
Vladislav Voloshin <voloshinw@gmail.com>
2021-07-19 11:11:21 +0300

    deleted excess rows

--- StarNavi/imdb/models.py
+++ StarNavi/imdb/models.py
@@ -48,4 +48,3 @@ class Likes(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
-
